3d195dfe259ab225bd067b9c4eb9a18d2c8f0f54
udaykompella <102951522+udaykompella@users.noreply.github.com>
2023-12-19 16:42:07 +0530

    assignmentfinal

---
+++ index.js
@@ -0,0 +1,11 @@
+// const radioButton = document.getElementById("radioButton");
+// const firstDiv = document.getElementById("firstDiv");
+// const secondDiv = document.getElementById("secondDiv");
+
+// radioButton.addEventListener("change", function () {
+//   if (this.checked) {
+//     secondDiv.style.display = "block";
+//   } else {
+//     secondDiv.style.display = "none";
+//   }
+// });
